f4d69808ffd60fe980bff9823caedeab57f6ef5d
unknown <cjdrangel209@gmail.com>
2022-02-24 08:54:27 +0000

    print statements for boundary tests for box and rectangle testlets

--- Lab6Part2/src/main/java/pckg/BoxtTestlet.java
+++ Lab6Part2/src/main/java/pckg/BoxtTestlet.java
@@ -12,6 +12,18 @@ public class BoxtTestlet implements TestletIF{
     Box b = new Box();
     
     public void runTest(TestHarness test){
-        test.checkEqual(b.getVolume(1, 1, 1), 1);
+        System.out.println("Box l = 1, w = 5, h = 5; expected volume: 25; result: "  + test.checkEqual(b.getVolume(1, 5, 5), 25));
+        System.out.println("Box l = 2, w = 5, h = 5; expected volume: 50; result: "  + test.checkEqual(b.getVolume(2, 5, 5), 50));
+        System.out.println("Box l = 5, w = 5, h = 5; expected volume: 125; result: "  + test.checkEqual(b.getVolume(5, 5, 5), 125));
+        System.out.println("Box l = 9, w = 5, h = 5; expected volume: 225; result: "  + test.checkEqual(b.getVolume(9, 5, 5), 225));
+        System.out.println("Box l = 10, w = 5, h = 5; expected volume: 250; result: "  + test.checkEqual(b.getVolume(10, 5, 5), 250));
+        System.out.println("Box l = 5, w = 1, h = 5; expected volume: 25; result: "  + test.checkEqual(b.getVolume(5, 1, 5), 25));
+        System.out.println("Box l = 5, w = 2, h = 5; expected volume: 50; result: "  + test.checkEqual(b.getVolume(5, 2, 5), 50));
+        System.out.println("Box l = 5, w = 9, h = 5; expected volume: 225; result: "  + test.checkEqual(b.getVolume(5, 9, 5), 225));
+        System.out.println("Box l = 5, w = 10, h = 5; expected volume: 250; result: "  + test.checkEqual(b.getVolume(5, 10, 5), 250));
+        System.out.println("Box l = 5, w = 5, h = 1; expected volume: 25; result: "  + test.checkEqual(b.getVolume(5, 5, 1), 25));
+        System.out.println("Box l = 5, w = 5, h = 2; expected volume: 50; result: "  + test.checkEqual(b.getVolume(5, 5, 2), 50));
+        System.out.println("Box l = 5, w = 5, h = 9; expected volume: 225; result: "  + test.checkEqual(b.getVolume(5, 5, 9), 225));
+        System.out.println("Box l = 5, w = 5, h = 10; expected volume: 250; result: "  + test.checkEqual(b.getVolume(5, 5, 10), 250));
     }
 }
--- Lab6Part2/src/main/java/pckg/RectangleTestlet.java
+++ Lab6Part2/src/main/java/pckg/RectangleTestlet.java
@@ -12,6 +12,14 @@ public class RectangleTestlet implements TestletIF{
     Rectangle r = new Rectangle();
     
     public void runTest(TestHarness test){
-        test.checkEqual(r.getArea(10, 10), 100);
+        System.out.println("Rectangle w = 1, h = 5; expected area: 5; result: " + test.checkEqual(r.getArea(1, 5), 5));
+        System.out.println("Rectangle w = 2, h = 5; expected area: 10; result: " + test.checkEqual(r.getArea(2, 5), 10));
+        System.out.println("Rectangle w = 5, h = 5; expected area: 25; result: " + test.checkEqual(r.getArea(5, 5), 25));
+        System.out.println("Rectangle w = 9, h = 5; expected area: 45; result: " + test.checkEqual(r.getArea(9, 5), 45));
+        System.out.println("Rectangle w = 10, h = 5; expected area: 50; result: " + test.checkEqual(r.getArea(10, 5), 50));
+        System.out.println("Rectangle w = 5, h = 1; expected area: 5; result: " + test.checkEqual(r.getArea(5, 1), 5));
+        System.out.println("Rectangle w = 5, h = 2; expected area: 10; result: " + test.checkEqual(r.getArea(5, 2), 10));
+        System.out.println("Rectangle w = 5, h = 9; expected area: 45; result: " + test.checkEqual(r.getArea(5, 9), 45));
+        System.out.println("Rectangle w = 5, h = 10; expected area: 50; result: " + test.checkEqual(r.getArea(5, 10), 50));
     }
 }
